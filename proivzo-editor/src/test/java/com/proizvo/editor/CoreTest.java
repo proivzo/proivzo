13bd75d604b188324ccd63f2abaf644d10b2715f
Hubertus Sack <hubertussack@outlook.com>
2016-07-09 01:10:54 +0200

    Added some test dependencies

---
+++ proivzo-editor/src/test/java/com/proizvo/editor/CoreTest.java
@@ -0,0 +1,33 @@
+package com.proizvo.editor;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.util.*;
+
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoreTest {
+
+	private static final Logger log = LoggerFactory.getLogger(CoreTest.class);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+	}
+
+	@Test
+	public void testSth() {
+		assertTrue(true);
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+	}
+}
