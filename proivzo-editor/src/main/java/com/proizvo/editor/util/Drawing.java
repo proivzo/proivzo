f4a803e9c8ce78a8af60159a1157a0b147a70c77
Hubertus Sack <hubertussack@outlook.com>
2016-07-10 15:50:40 +0200

    Added combining of images

--- proivzo-editor/src/main/java/com/proizvo/editor/util/Drawing.java
+++ proivzo-editor/src/main/java/com/proizvo/editor/util/Drawing.java
@@ -4,6 +4,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.awt.AlphaComposite;
+import java.awt.Composite;
 
 public class Drawing {
 
@@ -19,5 +22,18 @@ public class Drawing {
         g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         return g;
+    }
+
+    public static Image combine(Image ground, Image overlay, float alpha) {
+        int width = Math.max(ground.getWidth(null), overlay.getWidth(null));
+        int height = Math.max(ground.getHeight(null), overlay.getHeight(null));
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = create2D(img);
+        g.drawImage(ground, 0, 0, width, height, null);
+        int rule = AlphaComposite.SRC_OVER;
+        Composite comp = AlphaComposite.getInstance(rule, alpha);
+        g.setComposite(comp);
+        g.drawImage(overlay, 0, 0, width, height, null);
+        return img;
     }
 }
