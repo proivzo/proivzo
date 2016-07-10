6c10ba6229da59e8d2b7f137f5ae02165f4dcb86
Hubertus Sack <hubertussack@outlook.com>
2016-07-10 14:45:00 +0200

    Extended color options

--- proivzo-editor/src/main/java/com/proizvo/editor/tiles/ColorTile.java
+++ proivzo-editor/src/main/java/com/proizvo/editor/tiles/ColorTile.java
@@ -14,13 +14,19 @@ public class ColorTile {
     }
 
     public static Image drawHalfed(String firstColor, String secondColor) {
+        Color first = Color.decode("#" + firstColor);
+        Color second = Color.decode("#" + secondColor);
+        return drawHalfed(first, second);
+    }
+
+    public static Image drawHalfed(Color firstColor, Color secondColor) {
         BufferedImage img = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = img.createGraphics();
         int half = tileSize / 2;
-        g.setColor(Color.decode("#" + firstColor));
+        g.setColor(firstColor);
         g.fillRect(0, 0, half, half);
         g.fillRect(half, half, half, half);
-        g.setColor(Color.decode("#" + secondColor));
+        g.setColor(secondColor);
         g.fillRect(0, half, half, half);
         g.fillRect(half, 0, half, half);
         g.dispose();
