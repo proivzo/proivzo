affc25a09321112644b5071cf70547cdb88478cf
Hubertus Sack <hubertussack@outlook.com>
2016-07-10 16:52:52 +0200

    Added drawing of centered strings

--- proivzo-editor/src/main/java/com/proizvo/editor/util/Drawing.java
+++ proivzo-editor/src/main/java/com/proizvo/editor/util/Drawing.java
@@ -6,7 +6,10 @@ import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Composite;
+import java.awt.Font;
+import java.awt.FontMetrics;
 
 public class Drawing {
 
@@ -34,6 +37,28 @@ public class Drawing {
         Composite comp = AlphaComposite.getInstance(rule, alpha);
         g.setComposite(comp);
         g.drawImage(overlay, 0, 0, width, height, null);
+        return img;
+    }
+
+    public static void drawCenteredStr(String txt, int w, int h, Font font, Color color, Graphics g) {
+        g.setColor(color);
+        g.setFont(font);
+        FontMetrics fm = g.getFontMetrics();
+        int x = (w - fm.stringWidth(txt)) / 2;
+        int y = (fm.getAscent() + (h - (fm.getAscent() + fm.getDescent())) / 2);
+        g.drawString(txt, x, y);
+    }
+
+    public static void drawCenteredStr(String txt, int w, int h, int fontSize, Color color, Graphics g) {
+        Font font = g.getFont().deriveFont(Font.BOLD, fontSize);
+        drawCenteredStr(txt, w, h, font, color, g);
+    }
+
+    public static Image addCenteredStr(String txt, Color color, int size, Image img) {
+        int w = img.getWidth(null);
+        int h = img.getHeight(null);
+        Graphics g = create2D(img);
+        drawCenteredStr(txt, w, h, size, color, g);
         return img;
     }
 }
