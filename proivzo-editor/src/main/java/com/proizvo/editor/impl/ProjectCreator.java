8700578edf7e4f1d35ac9f7910b53dd9cb1f8d50
husaft <hubertussack@outlook.com>
2016-07-28 08:48:44 +0200

    Project creation now includes index HTML

--- proivzo-editor/src/main/java/com/proizvo/editor/impl/ProjectCreator.java
+++ proivzo-editor/src/main/java/com/proizvo/editor/impl/ProjectCreator.java
@@ -1,9 +1,17 @@
 package com.proizvo.editor.impl;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.commons.io.FileUtils;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.dom4j.dom.DOMDocumentType;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.XMLWriter;
 
 import com.proizvo.editor.api.IProject;
 import com.proizvo.editor.util.Files;
@@ -18,6 +26,8 @@ public class ProjectCreator {
             String encoding = "UTF8";
             File gpf = new File(projDir, "Game.rpgproject");
             FileUtils.write(gpf, "RPGMV 1.0.0", encoding);
+            File idx = new File(projDir, "index.html");
+            writeIndex(idx, gameTitle);
             File audio = Files.mkdir(projDir, "audio");
             File bgm = Files.mkdir(audio, "bgm");
             File bgs = Files.mkdir(audio, "bgs");
@@ -49,5 +59,76 @@ public class ProjectCreator {
         } catch (IOException e) {
             throw new RuntimeException("Could not create project!", e);
         }
+    }
+
+    private static void write(Document doc, File file) {
+        try (FileWriter out = new FileWriter(file)) {
+            OutputFormat fmt = OutputFormat.createPrettyPrint();
+            fmt.setXHTML(true);
+            fmt.setSuppressDeclaration(true);
+            fmt.setExpandEmptyElements(true);
+            fmt.setIndentSize(4);
+            XMLWriter xml = new XMLWriter(out, fmt);
+            xml.write(doc);
+            xml.flush();
+            xml.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Could not write document!", e);
+        }
+    }
+
+    private static void writeIndex(File idx, String gameTitle) {
+        Document doc = DocumentHelper.createDocument();
+        doc.setDocType(new DOMDocumentType("html", null));
+        Element root = doc.addElement("html");
+        Element head = root.addElement("head");
+        addMeta(head, "UTF-8");
+        addMeta(head, "apple-mobile-web-app-capable", "yes");
+        addMeta(head, "apple-mobile-web-app-status-bar-style",
+                "black-translucent");
+        addMeta(head, "viewport", "user-scalable=no");
+        addLink(head, "icon", "icon/icon.png", "image/png");
+        addLink(head, "apple-touch-icon", "icon/icon.png", null);
+        addLink(head, "stylesheet", "fonts/gamefont.css", "text/css");
+        head.addElement("title").setText(gameTitle);
+        Element body = root.addElement("body");
+        body.addAttribute("style", "background-color: black");
+        for (String jsPath : Arrays.asList("js/libs/pixi.js",
+                "js/libs/fpsmeter.js", "js/libs/lz-string.js",
+                "js/rpg_core.js", "js/rpg_managers.js", "js/rpg_objects.js",
+                "js/rpg_scenes.js", "js/rpg_sprites.js", "js/rpg_windows.js",
+                "js/plugins.js", "js/main.js")) {
+            addScript(body, jsPath);
+        }
+        write(doc, idx);
+    }
+
+    private static Element addLink(Element root, String rel, String href,
+            String type) {
+        Element link = root.addElement("link");
+        link.addAttribute("rel", rel);
+        link.addAttribute("href", href);
+        link.addAttribute("type", type);
+        return link;
+    }
+
+    private static Element addMeta(Element root, String key, String value) {
+        Element meta = root.addElement("meta");
+        meta.addAttribute("name", key);
+        meta.addAttribute("content", value);
+        return meta;
+    }
+
+    private static Element addMeta(Element root, String charset) {
+        Element meta = root.addElement("meta");
+        meta.addAttribute("charset", charset);
+        return meta;
+    }
+
+    private static Element addScript(Element root, String path) {
+        Element script = root.addElement("script");
+        script.addAttribute("type", "text/javascript");
+        script.addAttribute("src", path);
+        return script;
     }
 }
