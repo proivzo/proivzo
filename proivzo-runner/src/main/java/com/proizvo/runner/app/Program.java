6fc362a819f6f48857cf25f30e1c923ad8f5d644
husaft <hubertussack@outlook.com>
2016-08-04 23:38:49 +0200

    Added project runner for easier game testing

---
+++ proivzo-runner/src/main/java/com/proizvo/runner/app/Program.java
@@ -0,0 +1,30 @@
+package com.proizvo.runner.app;
+
+import com.xafero.sfs.Hoster;
+import com.xafero.sfs.impl.PopupPlugin;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Program {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int width = 816;
+        int height = 624;
+        String title = "Proizvo";
+        File root = (new File("")).getAbsoluteFile();
+        Hoster host = new Hoster(root, new PopupPlugin(title, "index.html", title, width, height));
+        System.out.format("Publishing '%s' under '%s'... %n", root, host.getEndpoint());
+        ExecutorService pool = Executors.newCachedThreadPool();
+        pool.submit(host);
+        Thread.sleep(2 * 1000);
+        Desktop.getDesktop().browse(host.getEndpoint());
+        System.out.println();
+        System.out.println("Press any key to exit...");
+        System.in.read();
+        pool.shutdown();
+        System.exit(0);
+    }
+}
